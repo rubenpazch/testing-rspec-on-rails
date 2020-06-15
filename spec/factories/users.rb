18c0fc24c4e1c6f73acfe0f3c2a6e2b7824c91b1
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-06-14 20:08:22 -0500

    test cases for user with factories and faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username { 'MyString' }
-    email { 'MyString' }
+    username { Faker::Alphanumeric.alphanumeric(number: 20) }
+    email { Faker::Internet.email }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe ' Model.User' do
     fixtures :users
+    let(:user){FactoryBot.create :user}
 
-    context 'validates field' do
+    context 'validates field using fixtures' do
       it 'username must be valid' do
         expect(users(:one)).to be_valid
       end
@@ -19,7 +20,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context 'username must include the message' do
+    context 'username must include the message using fixtures' do
       it 'must be given ' do
         users(:one).username = nil
         users(:one).valid?
@@ -33,7 +34,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    context 'username must include the message' do
+    context 'username must include the message using fixtures' do
       it 'length between 6 to 20 characters' do
         users(:one).username = '123abcgjhasjdhasjkdhasjdkhasjdkhasdjkas'
         users(:one).valid?
@@ -43,6 +44,48 @@ RSpec.describe User, type: :model do
         users(:one).username = ' ruben22123123'
         users(:one).valid?
         expect(users(:one).errors[:username]).to include('Username must be alphanumeric')
+      end
+    end
+
+    context 'validates field using factories and faker' do
+      it 'username must be valid' do
+        expect(user).to be_valid
+      end
+      it 'username must not be valid' do
+        user.username = nil
+        expect(user).to_not be_valid
+      end
+      it 'validates user already exists' do
+        user_new = User.new(username: 'juanperez')
+        user_new.valid?
+        expect(user_new.errors[:username]).to include('Username already exists')
+      end
+    end
+
+    context 'username must include the message using factories and faker' do
+      it 'must be given ' do
+        user.username = nil
+        user.valid?
+        expect(user.errors[:username]).to include('Username must be given')
+      end
+
+      it 'length between 6 to 20 characters' do
+        user.username = '123'
+        user.valid?
+        expect(user.errors[:username]).to include('Username length must be between 6 to 20 characters')
+      end
+    end
+
+    context 'username must include the message using factories and faker' do
+      it 'length between 6 to 20 characters' do
+        user.username = '123abcgjhasjdhasjkdhasjdkhasjdkhasdjkas'
+        user.valid?
+        expect(user.errors[:username]).to include('Username length must be between 6 to 20 characters')
+      end
+      it 'must be alphanumeric' do
+        user.username = ' ruben22123123'
+        user.valid?
+        expect(user.errors[:username]).to include('Username must be alphanumeric')
       end
     end
   end
